585d6b2f38a9cb6cbc17e62d92b2b7b8ef299bac
jniyitegek <j.niyitegek@alustudent.com>
2025-03-03 15:47:52 +0200

    printing concatenated strings

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str_text[str_text.find("object-oriented"):str_text.find("programming") + len("programming")] + str_text[str_text.rfind("Python") - 5:])
+
+str = str[str.index("object"):str.index("programming") + len("programming")] + str[str.index("with"):]
+print(str)
