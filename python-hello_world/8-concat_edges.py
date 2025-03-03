f6cfd9532a88a1b4dfb1966298abf64862a15932
jniyitegek <j.niyitegek@alustudent.com>
2025-03-03 16:43:18 +0200

    printing concatenated strings

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find(str.split()[5]):str.find(str.split()[7])] + str[str.find(str.split()[12]):str.find(str.split()[-len(str.split())+1])]
+str = str[str.find(str.split()[5]):str.find(str.split()[7])] + str[str.find(str.split()[12]):str.find(str.split()[13])] + str[:str.find(str.split()[1])]
 print(str)
