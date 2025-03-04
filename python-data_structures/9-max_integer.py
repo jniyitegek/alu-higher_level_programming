d0acc1a8ae24f6065355322e49a00b5cc296c60b
jniyitegek <j.niyitegek@alustudent.com>
2025-03-04 23:28:25 +0200

    the task is part of the list and tuples modules and it is completed successfuly

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
+
     largest = my_list[0]
     for num in my_list[1:]:
         if num > largest:
             largest = num
-    
+
     return largest
