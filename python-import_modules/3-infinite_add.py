57b62fdf3b61503568d76e700cd7d9b213aa9473
jniyitegek <j.niyitegek@alustudent.com>
2025-03-04 22:33:15 +0200

    this is part of project three of imports

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-# Sum all arguments starting from index 1 (skipping script name)
-total = sum(int(arg) for arg in sys.argv[1:])
 
-# Print the total sum
-print(total)
+def main():
+    # Sum all arguments starting from index 1 (skipping script name)
+    total = sum(int(arg) for arg in sys.argv[1:])
+
+    # Print the total sum
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
