cdea2a47da5502e2d3e067eb7200eda3efde0691
jniyitegek <j.niyitegek@alustudent.com>
2025-03-04 22:37:15 +0200

    this is part of project three of imports

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,9 @@ def main():
     names = dir(hidden_4)
 
     # Filter and sort names that don't start with '__'
-    filtered_names = sorted(name for name in names if not name.startswith('__'))
+    filtered_names = sorted(
+        name for name in names if not name.startswith('__')
+    )
 
     # Print each name
     for name in filtered_names:
