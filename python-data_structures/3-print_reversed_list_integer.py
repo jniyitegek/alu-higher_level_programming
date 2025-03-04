e766a2ef353a2b19045cba41c5512457e4727c92
jniyitegek <j.niyitegek@alustudent.com>
2025-03-04 23:49:37 +0200

    the task is part of the list and tuples modules and it is completed successfuly

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return  # Exit the function if the list is None
+
     for num in reversed(my_list):
         print("{:d}".format(num))
