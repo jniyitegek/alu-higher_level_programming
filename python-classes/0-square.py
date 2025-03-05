4800b3e28941e8104c06b2ad76cefd261b29bc63
jniyitegek <j.niyitegek@alustudent.com>
2025-03-05 22:39:42 +0200

    classes and instances of classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+"""
+Module Name: square.py
+
+This module defines a Square class that represents a geometric square.
+
+Author: Jean Philippe Niyitegeka
+Date: March 5, 2025
+Version: 1.0
+
+Usage:
+    from square import Square
+    my_square = Square()
+"""
+
 class Square:
+    """
+    A class that defines a square.
+    
+    This is an empty class that serves as a foundation for implementing
+    square-related functionality in future iterations.
+    """
     pass
