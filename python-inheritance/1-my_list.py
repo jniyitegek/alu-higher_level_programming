6dfad5c9e9df5b9d5d79d27d9a017cc65fefbf13
jniyitegek <j.niyitegek@alustudent.com>
2025-03-06 15:17:15 +0200

    python inhertance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for MyList class that inherits from list."""
+
+class MyList(list):
+    """A class that inherits from list and provides additional functionality."""
+
+    def print_sorted(self):
+        """Print the list in ascending sorted order."""
+        print(sorted(self))
