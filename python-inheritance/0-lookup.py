020bc42f7dd86f9bda54c5304349a97fbb96b315
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-03-06 14:50:54 +0200

    inhertance python task 1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,10 +2,14 @@
 def lookup(obj):
     """Returns a list of available attributes and methods of an object.
 
+    This function uses the built-in `dir()` function to retrieve the attributes
+    and methods of the given object.
+
     Args:
-        obj: The object to inspect.
+        obj (object): The object to inspect.
 
     Returns:
-        list: A list of strings representing the attributes and methods of the object.
+        list: A list of strings representing the names of the attributes and methods
+              of the object.
     """
     return dir(obj)
