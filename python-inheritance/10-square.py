bb1e631613a04bb8b9cc92c51cb6f357647c674f
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-03-06 23:06:19 +0200

    Update 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -40,7 +40,7 @@ class Rectangle:
             TypeError: If value is not an integer.
             ValueError: If value is not greater than 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
@@ -73,30 +73,4 @@ class Square(Rectangle):
         Initializes a square with a given size.
 
         Args:
-            size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is not greater than 0.
-        """
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        """
-        Returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
-
-    def __str__(self):
-        """
-        Returns a string representation of the square.
-
-        Returns:
-            str: The string representation of the square.
-        """
-        return f"[Square] {self.__size}/{self.__size}"
+            size (int): The size
