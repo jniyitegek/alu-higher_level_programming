809968fecd843ef0031b3072b9e49b6ef4752c8f
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-03-06 15:05:45 +0200

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -5,6 +5,13 @@ This module defines a Square class that inherits from the Rectangle class
 and represents a square with a given size. The size is validated to ensure
 it is a positive integer, and the class provides methods for calculating
 the area and returning a string representation of the square.
+
+Example:
+    To use the Square class, instantiate it with a size and call its methods:
+
+        s = Square(5)
+        print(s)        # Output: [Square] 5/5
+        print(s.area()) # Output: 25
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
