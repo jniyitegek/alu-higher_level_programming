b3ee829d7cd7b97a90c8b725115bd5c2d4628d8a
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-03-06 16:44:31 +0200

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Module for is_kind_of_class function."""
 
+
 def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance of, or inherited from, the specified class.
+    """Check if an object is an instance of, or from, the specified class.
 
     Args:
         obj: The object to check.
         a_class: The class to compare against.
 
     Returns:
-        bool: True if `obj` is an instance of or inherited from `a_class`; otherwise False.
+        bool: True if `obj` is an instance from `a_class`; otherwise False.
     """
     return isinstance(obj, a_class)
