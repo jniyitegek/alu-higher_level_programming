18702dac2bdc99c97acca2cd65160a479cc819bc
jniyitegek <j.niyitegek@alustudent.com>
2025-04-04 23:03:49 +0200

    task 4:

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request with a custom header and display the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+#send a GET request to the URL and display response
+curl -sG -H "X-School-User-Id: 98" "$1"
